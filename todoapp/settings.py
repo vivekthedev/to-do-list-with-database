1cda3375f0e067cd0a5f0affcce879c83f19b85c
Vivek Kumar Singh <71426368+vivekthedev@users.noreply.github.com>
2022-02-03 13:16:21 +0530

    Update settings.py

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nv8d#mkw!peb=qcl4@lz_4qv(^g$6yu+uscmsdee1-m_unfi##'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['taskstokill.herokuapp.com']
 
